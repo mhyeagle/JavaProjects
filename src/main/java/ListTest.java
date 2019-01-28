8339df96ef66874dc898021947c79203b7874804
tianyashuibin <tianyashuibin@163.com>
2019-01-28 20:20:35 +0800

    add list iterator

---
+++ src/main/java/ListTest.java
@@ -0,0 +1,36 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ListTest {
+    private static final Logger logger = LoggerFactory.getLogger(ListTest.class);
+
+    public static void main(String[] args) {
+        LinkedList<String> words = new LinkedList<>();
+
+        words.add("a");
+        words.add("b");
+        words.add("c");
+
+        Iterator it = words.iterator();
+
+//        int num = 0;
+//        while (it.hasNext()) {
+//            logger.info("{}", it.next());
+//            if (num == 2) {
+//                words.add("2");
+//            }
+//            num++;
+//        }
+
+        for (int i = 0; i < words.size(); i++) {
+            if (i == 2) {
+                words.add("2");
+                words.set(2, "1024");
+            }
+            logger.info("{}", words.get(i));
+        }
+    }
+}
