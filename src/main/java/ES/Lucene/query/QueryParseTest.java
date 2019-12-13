2146cd7be9dc8bd81a2c730f7565eef2bb6e586b
tianyashuibin <tianyashuibin@163.com>
2019-12-13 15:10:34 +0800

    Lucene query parse

--- src/main/java/ES/Lucene/query/QueryParseTest.java
+++ src/main/java/ES/Lucene/query/QueryParseTest.java
@@ -3,14 +3,14 @@ package ES.Lucene.query;
 import ES.Lucene.IK.IKAnalyzer6x;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.IntPoint;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
@@ -26,10 +26,53 @@ public class QueryParseTest {
         Directory directory = FSDirectory.open(indexPath);
         IndexReader reader = DirectoryReader.open(directory);
         IndexSearcher searcher = new IndexSearcher(reader);
+
         Analyzer analyzer = new IKAnalyzer6x();
-        QueryParser parser = new QueryParser(field, analyzer);
-        parser.setDefaultOperator(QueryParser.Operator.AND);
-        Query query = parser.parse("农村学生"); // 查询关键词
+
+//        QueryParser parser = new QueryParser(field, analyzer);
+//        parser.setDefaultOperator(QueryParser.Operator.AND);
+//        Query query = parser.parse("农村学生"); // 查询关键词
+
+//        // 多域搜索（MultiFieldQueryParser）
+//        String[] fields = {"title", "content"};
+//        MultiFieldQueryParser multiFieldQueryParser = new MultiFieldQueryParser(fields, analyzer);
+//        Query query = multiFieldQueryParser.parse("美国");
+
+//        // 词项搜索（TermQuery)
+//        Term term = new Term("title", "美国");
+//        Query query = new TermQuery(term);
+
+        // 布尔搜索（BooleanQuery)
+//        Query query1 = new TermQuery(new Term("title", "美国"));
+//        Query query2 = new TermQuery(new Term("content", "日本"));
+//        BooleanClause booleanClause1 = new BooleanClause(query1, BooleanClause.Occur.MUST);
+//        BooleanClause booleanClause2 = new BooleanClause(query2, BooleanClause.Occur.MUST_NOT);
+//        BooleanQuery query = new BooleanQuery.Builder()
+//                .add(booleanClause1)
+//                .add(booleanClause2)
+//                .build();
+
+        // 范围搜索（RangeQuery）
+//        Query query = IntPoint.newRangeQuery("reply", 500, 1000);
+
+        // 前缀搜索（PrefixQuery）
+        // 查询包含以"学"开头的词项的文档
+//        Term term = new Term("title", "学");
+//        Query query = new PrefixQuery(term);
+
+        // 多关键字搜索（PhraseQuery）
+//        PhraseQuery.Builder builder = new PhraseQuery.Builder();
+//        builder.add(new Term("title", "日本"), 2);
+//        builder.add(new Term("title", "美国"), 3);
+//        PhraseQuery query = builder.build();
+
+        // 模糊搜索（FuzzyQuery）
+//        Term term = new Term("title", "Tramp");
+//        FuzzyQuery query = new FuzzyQuery(term);
+
+        // 通配符搜索（WildcardQuery）
+        WildcardQuery query = new WildcardQuery(new Term(field, "学?"));
+
         System.out.println("Query:" + query.toString());
         // 返回前10条
         TopDocs topDocs = searcher.search(query, 10);
@@ -42,5 +85,6 @@ public class QueryParseTest {
         }
         directory.close();
         reader.close();
+
     }
 }
