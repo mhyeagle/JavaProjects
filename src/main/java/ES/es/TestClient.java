c9380388c39d0091f2122bd3f9ec10ef374cd63f
tianyashuibin <tianyashuibin@163.com>
2019-12-21 12:27:48 +0800

    elasticsearch rest high level client test

---
+++ src/main/java/ES/es/TestClient.java
@@ -0,0 +1,32 @@
+package ES.es;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+
+import java.io.IOException;
+
+public class TestClient {
+    public static String HOST = "localhost";
+    public static int HTTP_PORT = 9200;
+
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost(HOST, HTTP_PORT)
+                )
+        );
+
+        GetRequest request = new GetRequest("books", "1");
+        String[] includes = new String[]{"title", "published"};
+        FetchSourceContext fetchSourceContext = new FetchSourceContext(true, includes, null);
+        request.fetchSourceContext(fetchSourceContext);
+
+        GetResponse response = restHighLevelClient.get(request, RequestOptions.DEFAULT);
+        System.out.println("response:" + response.getSourceAsString());
+    }
+}
